e8b4d5c7a143842632ed54461bf68994c07b4218
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-03 23:15:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,19 @@
-def square_array(numbers)
-   numbers=[1,2,3]
+def square_array(array)
    counter=0
-   while counter < numbers.length
-     (numbers.length[counter])
+   new_array=[]
+   while counter < array.length
+     new_array.push(array[count].to_s)
      counter +=1
   end
+  new_array
+end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
 end
