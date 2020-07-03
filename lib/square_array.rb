20e7b33c0f258c99c6889c52c75d8c6430c38d22
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-03 23:16:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
    counter=0
    new_array=[]
    while counter < array.length
-     new_array.push(array[count]**2).to_s
+     new_array.push(array[counter]**2).to_s
      counter +=1
   end
   new_array
