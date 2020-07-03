52bff09308fd50ee0682523f76fe79fe5ecc8b5e
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-03 23:13:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+   numbers=[1,2,3]
+   counter=0
+   while counter < numbers.length
+     (numbers.length[counter])
+     counter +=1
+  end
 end
